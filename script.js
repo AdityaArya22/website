b44394bfc4c3c56c5fee96ebd361d6e7e4cf6c06
AdityaArya22 <aryaaditya2207@gmail.com>
2024-05-08 22:32:14 +0530

    added animations

---
+++ script.js
@@ -0,0 +1,82 @@
+function locomotive() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    smooth: true
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy(".main", {
+    scrollTop(value) {
+      return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+  });
+
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+
+}
+locomotive()
+let tl = gsap.timeline()
+function responsive() {
+  let hamburger = document.querySelector(".hamburger");
+  let navItems = document.querySelector(".nav-items");
+  let open = false;
+  hamburger.addEventListener("click", () => {
+
+    if (open === true) {
+      // tl.reverse()
+      navItems.style.left = "800px"
+      open = false
+    }
+    else {
+      gsap.from(".nav-links", {
+        x: 100,
+        opacity:1,
+        duration:1,
+        stagger:0.15
+      })
+      navItems.style.left = "0px"
+      open = true
+    }
+  })
+}
+responsive()
+tl.from("header",{
+  opacity:0,
+  duration:1
+})
+tl.from(".socials i , .details",{
+  y:10,
+  opacity:0,
+  duration:2,
+  ease:"elastic",
+  stagger:0.2
+},"a")
+tl.from(".logo, .nav-links",{
+  y:10,
+  opacity:0,
+  duration:2,
+  ease:"elastic",
+  stagger:0.2
+},"a")
+tl.from(".bg-img img",{
+  x:100,
+  opacity:0,
+  duration:2,
+  ease:"power4"
+},"=+a")
